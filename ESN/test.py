3fb60ee62ecc8db9f9d9a18d578b4a55e9de7271
Daniele Di Sarli <danieleds0@gmail.com>
2018-11-04 18:19:06 +0100

    fix: use artificial dataset

--- ESN/test.py
+++ ESN/test.py
@@ -3,7 +3,7 @@ import math
 import random
 from esn import ESN
 
-dataset = torch.Tensor( [ math.sin(x*0.5) for x in range(2000) ] ) #torch.Tensor( [ math.sin(x*0.5) + 2 * round(math.cos(x*0.5)) for x in range(1000) ] )
+dataset = torch.Tensor( [ math.sin(x*0.5) + 2 * round(math.cos(x*0.5)) for x in range(2000) ] )
 dataset = dataset / dataset.abs().max()
 
 training_x = dataset[0:1200].view(-1,1)
@@ -11,14 +11,14 @@ training_y = dataset[1:1201]
 test_x = dataset[1200:-1].view(-1,1)
 test_y = dataset[1201:]
 
-model = ESN(1, 100, contractivity_coeff=1.2, density=0.4)
+model = ESN(1, 50, contractivity_coeff=1.2, density=0.9)
 
 # Collect states
 X = model(training_x)
 
 # Washout
-X = X[100:]
-Y = training_y[100:]
+X = X[200:]
+Y = training_y[200:]
 
 # Train the model
 W = X.pinverse() @ Y
