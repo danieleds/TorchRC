b8315cdc51f1d1dedb53c81c7c9313d076c72f36
Daniele Di Sarli <danieleds0@gmail.com>
2018-11-04 17:55:09 +0100

    feat: ESN implementation

---
+++ ESN/test.py
@@ -0,0 +1,31 @@
+import torch
+import math
+import random
+from esn import ESN
+
+dataset = torch.Tensor( [ math.sin(x*0.5) for x in range(2000) ] ) #torch.Tensor( [ math.sin(x*0.5) + 2 * round(math.cos(x*0.5)) for x in range(1000) ] )
+dataset = dataset / dataset.abs().max()
+
+training_x = dataset[0:1200].view(-1,1)
+training_y = dataset[1:1201]
+test_x = dataset[1200:-1].view(-1,1)
+test_y = dataset[1201:]
+
+model = ESN(1, 100, contractivity_coeff=1.2, density=0.4)
+
+# Collect states
+X = model(training_x)
+
+# Washout
+X = X[100:]
+Y = training_y[100:]
+
+# Train the model
+W = X.pinverse() @ Y
+
+# Test
+X_test = model(test_x)
+predicted_test = X_test @ W
+
+loss = torch.nn.MSELoss()
+print("MSE:", loss(test_y, predicted_test).item())
---
+++ README.md
@@ -0,0 +1,7 @@
+# PyTorch implementation of Reservoir Computing models
+
+## ESN
+
+Implementation of a basic Echo State Network without output feedback connections.
+
+ - LUKOŠEVIČIUS, Mantas; JAEGER, Herbert. Reservoir computing approaches to recurrent neural network training. Computer Science Review, 2009, 3.3: 127-149.
