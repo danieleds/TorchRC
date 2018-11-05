7228b43c0cb7f02c566cde845f29b31eaab47754
Daniele Di Sarli <danieleds0@gmail.com>
2018-11-05 11:29:09 +0100

    fix: add washout for test

--- ESN/test.py
+++ ESN/test.py
@@ -6,6 +6,8 @@ from esn import ESN
 dataset = torch.Tensor( [ math.sin(x*0.5) + 2 * round(math.cos(x*0.5)) for x in range(2000) ] )
 dataset = dataset / dataset.abs().max()
 
+washout = 200
+
 training_x = dataset[0:1200].view(-1,1)
 training_y = dataset[1:1201]
 test_x = dataset[1200:-1].view(-1,1)
@@ -17,8 +19,8 @@ model = ESN(1, 50, contractivity_coeff=1.2, density=0.9)
 X = model(training_x)
 
 # Washout
-X = X[200:]
-Y = training_y[200:]
+X = X[washout:]
+Y = training_y[washout:]
 
 # Train the model
 W = X.pinverse() @ Y
@@ -28,4 +30,4 @@ X_test = model(test_x)
 predicted_test = X_test @ W
 
 loss = torch.nn.MSELoss()
-print("MSE:", loss(test_y, predicted_test).item())
+print("MSE:", loss(test_y[washout:], predicted_test[washout:]).item())
