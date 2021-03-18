7e2cb98eed05015cb2ed3ad7c08a7e27b955eeb9
Daniele Di Sarli <danieleds0@gmail.com>
2021-03-18 11:07:24 +0100

    v0.2.1

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
+# read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="torch_rc",
-    version="0.2",
+    version="0.2.1",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         "torch",
     ],
